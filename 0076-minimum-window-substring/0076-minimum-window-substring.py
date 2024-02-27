ff6f5303a4039e4b26f54acbab726a5f15a9450b
jenhuang0 <70675492+jenhuang0@users.noreply.github.com>
2024-02-27 22:34:15 +0800

    Time: 89 ms (77.04%), Space: 17.2 MB (83.6%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,38 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # the substring need to at least pass the total char number of t. 
-        # and after that we need to narrow down the substring as small as we can
-        # track the index and the len of that substring 
-
-        if t == "": 
-            return ""
         target, sub = {}, {}
-        
-        #using dict to store the each char number in t
+
         for c in t:
             target[c] = target.get(c, 0) +1
-        
-        #using two variable to store the require char number from target, and substring char number
-        req_num, curr_num = len(target), 0
-
-        res_num = [-1,-1]
-        res_len = float("infinity") 
-
+        curr_num, need_num = 0, len(target) #this is by char not nums of each char
+        res_len = float("infinity")
+        res_index = [-1,-1]
         l = 0
         for r in range(len(s)):
-            c=s[r]
+            c = s[r]
             sub[c] = sub.get(c, 0) +1
-            if c in target and target[c] == sub[c]:
-                curr_num += 1
-            #update result
-            while req_num == curr_num:
-                if (r-l+1) < res_len:
-                    res_len = r-l+1
-                    res_num = [l, r]
-                #poping out from the left
-                sub[s[l]] -=1
+            if (c in target and target[c] == sub[c] ):
+                curr_num+=1
+            
+            while curr_num == need_num:
+                #update res
+                while (r - l +1) <res_len:
+                    res_len= r-l+1
+                    res_index= [l,r]
+                #pop from left to minix the len 
+                sub[s[l]]-=1
                 if s[l] in target and sub[s[l]] < target[s[l]]:
-                    curr_num -=1
+                    curr_num-=1
                 l+=1
-        l, r = res_num
-        return s[l:r+1] if res_len!=("infinity") else ""   
+        l, r = res_index
+        return s[l:r+1]     
