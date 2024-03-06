15dbde55903500125eefb98af405f2d3e1940788
jenhuang0 <70675492+jenhuang0@users.noreply.github.com>
2024-03-07 00:26:42 +0800

    Time: 29 ms (94.93%), Space: 16.8 MB (56.49%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,32 +1,23 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        #only add open par if open < n
-        #only add close par if close<open
-        #valid open == close == n
-
-        stack  = []
+        #only add open when it's less than n
+        #only add close when it's close< open
         ans = []
+        stack = []
 
-        def backtrack(openN, closeN):
-            if openN == closeN ==n:
-                a = ''
-                for i in stack:
-                    a += i
-                ans.append(a)
-                #ans.append("".join(stack))
-            if openN < n:
-                stack.append("(")
-                print(stack)
-                backtrack (openN+1, closeN)
-                stack.pop()
-                print(stack)
-                
-            if closeN < openN:
-                stack.append(")")
-                backtrack (openN, closeN+1)
-                stack.pop()
+        def backtrack(open, close):
             
-        backtrack(0, 0)
-
+            if open == close == n:
+                ans.append("".join(stack))
+            if open < n:
+                stack.append('(')
+                backtrack(open+1, close)
+                stack.pop()
+            if close< open:
+                stack.append(')')
+                backtrack(open, close+1)
+                stack.pop()
+        backtrack(0,0)
         return ans
-        
+
+                
