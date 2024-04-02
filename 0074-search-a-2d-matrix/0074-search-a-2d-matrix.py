2d66b162a99cebb22f25e6606dd37b676aba477b
jenhuang0 <70675492+jenhuang0@users.noreply.github.com>
2024-04-02 23:20:07 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,28 +1,35 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        #we can first find which row we should be searching 
-        rows, cols = len(matrix), len(matrix[0])
-        top, bom = 0, rows-1
-        while top<=bom:
-            mid= (top+bom)//2
-            if target > matrix[mid][-1]:
-                top =mid+1
-            elif target < matrix[mid][0]:
-                bom = mid -1
-            else: 
+        #first we can choose which row has the target num
+        #after find the target row we can using bs to find the target in that row
+        # in first step we might also not about the find the target row (keep that in mind: return false)
+
+        row = len(matrix)
+        col = len(matrix[0])
+        top = 0
+        btm = row-1
+
+        while top<=btm:
+            mid = (top+btm)//2
+            if matrix[mid][0] > target:
+                btm = mid-1
+            elif matrix[mid][-1] < target:
+                top = mid+1
+            else:
                 break
-        if not(top<=bom):
-            return False
+        if not top<=btm:
+            return False #mean the target is not in any row
         
-        row = (top+bom)//2
-        l, r = 0, cols-1
+        row = (top+btm)//2
+        l = 0 
+        r = col -1
 
         while l<=r:
             mid = (l+r)//2
-            if matrix[row][mid] > target:
-                r = mid-1
-            elif matrix[row][mid] <target:
+            if matrix[row][mid]> target:
                 l = mid+1
+            elif matrix[row][mid] < target:
+                r = mid-1
             else:
                 return True
         return False
