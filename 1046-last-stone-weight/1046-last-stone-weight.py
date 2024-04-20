2639e31746730f6d160aaedc8d1da258f78ec57b
jenhuang0 <70675492+jenhuang0@users.noreply.github.com>
2024-04-21 02:11:12 +0800

    Time: 42 ms (16.02%), Space: 16.4 MB (89.99%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,19 +1,18 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        #we can use heapify and reverse it so the max will be the tree on top
         max_heap = []
         heapq.heapify(max_heap)
-        
+
         for stone in stones:
             heapq.heappush(max_heap, -stone)
-        
+        print(max_heap)
         while len(max_heap) > 1:
             x = heapq.heappop(max_heap)
             y = heapq.heappop(max_heap)
-
-            new_num = x - y
-
-            if new_num != 0:
-                heapq.heappush(max_heap, new_num)
-
-        return -heapq.heappop(max_heap) if max_heap else 0
+            push_back = x-y
+            heapq.heappush(max_heap, push_back)
+        if max_heap:
+            ans = heapq.heappop(max_heap)
+            return -ans
+        else:
+            return None
