7c0a9a700d705edcb86962737a70d29007b89b7e
jenhuang0 <70675492+jenhuang0@users.noreply.github.com>
2024-04-21 21:17:31 +0800

    Time: 474 ms (28.03%), Space: 17 MB (81.9%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,39 +1,18 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # have a max heap and a queue
-        # the max heap will start as the frequency of eahc task 
-        # and then will be popped from the heap and pushed to the queue
-        
-        # the queue will have the frequency left and the time it can be inserted again which is the current len (time) + n
-        
-        # since there is no max heap in python
-        # we will use a min heap and negative it's values
-        
-        frequencies = Counter(tasks).values()
-        
-        maxheap = []
-        heapq.heapify(maxheap)
-        
-        for freq in frequencies:
-            heapq.heappush(maxheap, freq * -1)
+        freq = Counter(tasks)
+        maxHeap = [ -f for f in freq.values()]
+        heapq.heapify(maxHeap)
 
-        ans = 0
-        q = deque()
-        
-        while maxheap or q:
-            
-            if q and q[0][0] == ans:
-                heapq.heappush(maxheap, q[0][1])
-                q.popleft()
-            
-            if maxheap:
-                current_task = heapq.heappop(maxheap)
-            
-            current_task += 1
-            ans += 1
-            
-            if current_task < 0:
-                q.append((ans+n, current_task))
-                
-        return ans
-        
+        q = deque() #pairs of [-f, idletime]
+        time = 0
+
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                f = 1 + heapq.heappop(maxHeap)
+                if f != 0:
+                    q.append([f, time+n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
